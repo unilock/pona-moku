0dd0dcd3295f782f17be452473b612abaf076a85
unilock <unilock@fennet.rentals>
2024-01-04 20:11:03 -0500

    Absolutely obliterate the HungerManager
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/io/github/nalathnidragon/pona_moku/mixin/HungerManagerMixin.java
+++ src/main/java/io/github/nalathnidragon/pona_moku/mixin/HungerManagerMixin.java
@@ -6,12 +6,49 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(HungerManager.class)
+@Mixin(value = HungerManager.class, priority = 9999)
 public abstract class HungerManagerMixin {
-	@Inject(method = "update",at=@At("HEAD"), cancellable = true)
+	@Inject(method = "update", at = @At("HEAD"), cancellable = true)
 	private void pona_moku$dontUpdate(PlayerEntity player, CallbackInfo ci)
 	{
 		ci.cancel();
+	}
+
+	@Inject(method = "add", at = @At("HEAD"), cancellable = true)
+	private void pona_moku$dontAdd(int food, float saturationModifier, CallbackInfo ci)
+	{
+		ci.cancel();
+	}
+
+	@Inject(method = "addExhaustion", at = @At("HEAD"), cancellable = true)
+	private void pona_moku$dontAddExhaustion(float exhaustion, CallbackInfo ci)
+	{
+		ci.cancel();
+	}
+
+	@Inject(method = "getFoodLevel", at = @At("RETURN"), cancellable = true)
+	private void pona_moku$fakeFoodLevel(CallbackInfoReturnable<Integer> cir)
+	{
+		cir.setReturnValue(20);
+	}
+
+	@Inject(method = "getPrevFoodLevel", at = @At("RETURN"), cancellable = true)
+	private void pona_moku$fakePrevFoodLevel(CallbackInfoReturnable<Integer> cir)
+	{
+		cir.setReturnValue(20);
+	}
+
+	@Inject(method = "getExhaustion", at = @At("RETURN"), cancellable = true)
+	private void pona_moku$fakeExhaustion(CallbackInfoReturnable<Float> cir)
+	{
+		cir.setReturnValue(null);
+	}
+
+	@Inject(method = "getSaturationLevel", at = @At("RETURN"), cancellable = true)
+	private void pona_moku$fakeSaturationLevel(CallbackInfoReturnable<Float> cir)
+	{
+		cir.setReturnValue(5.0F);
 	}
 }
